49449964f03fbf3d18c4b0806cd01faea9b3dc55
foxcom1 <fabio.oikawa@gmail.com>
2020-05-29 23:52:52 -0300

    Next step: random food and snake growth.

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function criarBG() {
 }
 
 function criarCobrinha(){
-    for(i=0; i < snake.length; i++){
+    for (i=0; i < snake.length; i++){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
@@ -32,17 +32,17 @@ function drawFood(){
 document.addEventListener("keydown", update);
 
 function update(event){
-    if((event.keyCode == 37) && (direction != "right")) direction = "left";
-    if((event.keyCode == 38) && (direction != "down")) direction = "up";
-    if((event.keyCode == 39) && (direction != "left")) direction = "right";
-    if((event.keyCode == 40) && (direction != "up")) direction = "down";
+    if ((event.keyCode == 37) && (direction != "right")) direction = "left";
+    if ((event.keyCode == 38) && (direction != "down")) direction = "up";
+    if ((event.keyCode == 39) && (direction != "left")) direction = "right";
+    if ((event.keyCode == 40) && (direction != "up")) direction = "down";
 }
 
 function iniciarJogo(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     criarBG();
     criarCobrinha();
@@ -56,7 +56,13 @@ function iniciarJogo(){
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX == food.x && snakeY == food.y){
+        food.x = Math.floor(Math.random() * 16) * box;
+        food.y = Math.floor(Math.random() * 16) * box;
+    }
+    else{
+        snake.pop();
+    }
 
     let newHead = {
         x: snakeX,
